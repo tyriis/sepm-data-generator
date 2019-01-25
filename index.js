95183afe7f8334fa665838084aaf47e15822d768
Nils Müller <nils.biesalski@gmail.com>
2019-01-25 11:09:58 +0100

    Change: added simple occupancy by month

--- index.js
+++ index.js
@@ -1,15 +1,141 @@
+let limit = 1000
+let occupancy = [
+    {
+        from: new Date('2018-02-01'),
+        to: new Date('2018-03-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2018-03-01'),
+        to: new Date('2018-04-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2018-04-01'),
+        to: new Date('2018-05-01'),
+        limit: limit * 0.7,
+    },
+    {
+        from: new Date('2018-05-01'),
+        to: new Date('2018-06-01'),
+        limit: limit * 0.7,
+    },
+    {
+        from: new Date('2018-06-01'),
+        to: new Date('2018-07-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2018-07-01'),
+        to: new Date('2018-08-01'),
+        limit: limit * 0.7,
+    },
+    {
+        from: new Date('2018-08-01'),
+        to: new Date('2018-09-01'),
+        limit: limit,
+    },
+    {
+        from: new Date('2018-09-01'),
+        to: new Date('2018-10-01'),
+        limit: limit * 0.85,
+    },
+    {
+        from: new Date('2018-10-01'),
+        to: new Date('2018-11-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2018-11-01'),
+        to: new Date('2018-12-01'),
+        limit: limit * 0.5,
+    },
+    {
+        from: new Date('2018-12-01'),
+        to: new Date('2019-01-01'),
+        limit: limit * 0.9,
+    },
+    {
+        from: new Date('2019-01-01'),
+        to: new Date('2019-02-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2019-02-01'),
+        to: new Date('2019-03-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2019-03-01'),
+        to: new Date('2019-04-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2019-04-01'),
+        to: new Date('2019-05-01'),
+        limit: limit * 0.7,
+    },
+    {
+        from: new Date('2019-05-01'),
+        to: new Date('2019-06-01'),
+        limit: limit * 0.7,
+    },
+    {
+        from: new Date('2019-06-01'),
+        to: new Date('2019-07-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2019-07-01'),
+        to: new Date('2019-08-01'),
+        limit: limit * 0.7,
+    },
+    {
+        from: new Date('2019-08-01'),
+        to: new Date('2019-09-01'),
+        limit: limit,
+    },
+    {
+        from: new Date('2019-09-01'),
+        to: new Date('2019-10-01'),
+        limit: limit * 0.85,
+    },
+    {
+        from: new Date('2019-10-01'),
+        to: new Date('2019-11-01'),
+        limit: limit * 0.6,
+    },
+    {
+        from: new Date('2019-11-01'),
+        to: new Date('2019-12-01'),
+        limit: limit * 0.5,
+    },
+    {
+        from: new Date('2019-12-01'),
+        to: new Date('2020-01-01'),
+        limit: limit * 0.9,
+    },
+    {
+        from: new Date('2020-01-01'),
+        to: new Date('2020-02-01'),
+        limit: limit * 0.6,
+    },
+]
+
+
+
 let roomCount = 140
-let from = new Date()
-let to = new Date(Date.now() + (1000 * 60 * 60 * 24 * 365))
+let from = new Date('2019-02-01');
+//let to = new Date(Date.now() + (1000 * 60 * 60 * 24 * 365))
+let to = new Date('2019-03-01');
 let maxDays = 21
 let minDays = 1
-let limit = 1000
 let bookings = []
 let minPrice = 5000
 let maxPrice = 25000
 
-const DAY = 8.64e7;
-const fs = require('fs');
+const DAY = 8.64e7
+const fs = require('fs')
 
 let rooms = []
 
@@ -20,31 +146,31 @@ for (let i = 0; i < roomCount; i++) {
 const casual = require('casual')
 
 function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
+    return Math.floor(Math.random()*(max-min+1)+min)
 }
 
 function daysBetween(one, another) {
-    return Math.round(Math.abs((+one) - (+another))/DAY);
+    return Math.round(Math.abs((+one) - (+another))/DAY)
 }
 
 function getFromTo() {
     let fromTime = randomIntFromInterval(from.getTime(), to.getTime())
-    let toTime = randomIntFromInterval(fromTime, (fromTime + (DAY * maxDays)));
-    let fromDate = new Date(fromTime);
-    fromDate.setHours(10);
-    fromDate.setMinutes(0);
-    fromDate.setSeconds(1);
-    fromDate.setMilliseconds(0);
-    let toDate = new Date(toTime);
-    toDate.setHours(10);
-    toDate.setMinutes(0);
-    toDate.setSeconds(0);
-    toDate.setMilliseconds(0);
+    let toTime = randomIntFromInterval(fromTime, (fromTime + (DAY * maxDays)))
+    let fromDate = new Date(fromTime)
+    fromDate.setHours(0)
+    fromDate.setMinutes(0)
+    fromDate.setSeconds(1)
+    fromDate.setMilliseconds(0)
+    let toDate = new Date(toTime)
+    toDate.setHours(0)
+    toDate.setMinutes(0)
+    toDate.setSeconds(0)
+    toDate.setMilliseconds(0)
     return {from: fromDate, to: toDate}
 }
 
 function getBasePrice(roomId) {
-    return randomIntFromInterval(minPrice, maxPrice);
+    return randomIntFromInterval(minPrice, maxPrice)
 }
 
 function isAvailable(room, from, to) {
@@ -53,10 +179,10 @@ function isAvailable(room, from, to) {
     for (let booking of room.bookings) {
         if ((fromTime >= booking.from && fromTime <= booking.to) // fromTime between from and to
         || (toTime >= booking.from && toTime <= booking.to)) {// toTime between from and to
-            return false;
+            return false
         }
     }
-    return true;
+    return true
 
 }
 
@@ -65,53 +191,72 @@ function bookRoom(room, from, to) {
 }
 
 function getAvailableRoom(from, to) {
-    let tested = new Set([]);
+    let tested = new Set([])
     let candidates = Array.apply(null, {length: roomCount}).map(Number.call, Number)
-    let room = null;
-    let i = 0;
+    let room = null
+    let i = 0
     while (room == null && i < roomCount) {
-        i++;
-        candidates = candidates.filter(item => !tested.has(item));
+        i++
+        candidates = candidates.filter(item => !tested.has(item))
         let index = randomIntFromInterval(0, candidates.length -1)
-        let candidate = rooms[index];
-        tested.add(index);
+        let candidate = rooms[index]
+        tested.add(index)
         if (isAvailable(candidate, from, to)) {
-            room = candidate;
+            room = candidate
         }
     }
-    return room;
+    return room
 }
 
-for (let i = 1; i <= limit; i++) {
-    let loopCount = 0;
-    let room = null;
-    while (room == null && loopCount < 1000) {
-        loopCount++;
-        let {from, to} = getFromTo()
-        room = getAvailableRoom(from, to)
+function generateBookings(limit) {
+    let breakLimit = 0
+    for (let i = 1; i <= limit && breakLimit < 10; i++) {
+        let loopCount = 0
+        let room = null
+        while (room == null && loopCount < 1000) {
+            let {from, to} = getFromTo()
+            let days = daysBetween(from, to)
+            if (days < minDays) {
+                continue
+            }
+            loopCount++
+            room = getAvailableRoom(from, to)
+            if (room == null) {
+                continue
+            }
+            bookRoom(room, from, to)
+            from.setSeconds(0)
+            bookings.push({
+                name: casual.full_name,
+                from: from,
+                to: to,
+                days: days,
+                room: room.number,
+                price: parseInt((getBasePrice(room) * days) / 100) * 100
+            })
+            console.log(`booking ${bookings.length}: room find took ${loopCount} tries`)
+        }
         if (room == null) {
-            continue;
+            breakLimit++;
+            console.log(`break after ${loopCount} iterations`)
         }
-        bookRoom(room, from, to);
-        let days = daysBetween(from, to)
-        bookings.push({
-            name: casual.full_name,
-            from: from,
-            to: to,
-            days: days,
-            room: room.number,
-            price: getBasePrice(room) * days
-        });
-        //console.log(`booking ${bookings.length}: room find took ${loopCount} tries`);
     }
-
+    if (breakLimit > 10) {
+        console.log(`breakLimit reached!!!`)
+    }
 }
+for (let item of occupancy) {
+    from = item['from']
+    to = item['to']
+    generateBookings(item['limit'])
+}
+
 
 function formatTwoChars(value) {
     if (value >= 10) {
-        return `${value}`;
+        return `${value}`
     }
-    return `0${value}`;
+    return `0${value}`
 }
 
 function formatDate(date) {
@@ -120,25 +265,41 @@ function formatDate(date) {
     let day = formatTwoChars(date.getDate())
     return `${year}-${month}-${day} 00:00:00.0`
 }
-
+/*
 let sql = "INSERT INTO BOOKING\n SELECT * FROM (\n    SELECT * FROM BOOKING WHERE FALSE\n"
 let n = 0
 for (booking of bookings) {
     n++
     sql = sql + `  UNION SELECT ${n}, ${2000+n}, NULL, ${booking.room}, '${booking.name}', NOW(), FALSE, '${formatDate(booking.from)}', '${formatDate(booking.to)}'\n`
 }
-sql += '  )\nWHERE NOT EXISTS(SELECT * FROM BOOKING);\n'
+sql += '  )\nWHERE NOT EXISTS(SELECT * FROM BOOKING)\n'
 sql += '\nINSERT INTO BOOKING_POSTIONS\nSELECT  * FROM (\n    SELECT * FROM BOOKING_POSTIONS WHERE FALSE\n'
-n = 0;
+n = 0
 for (booking of bookings) {
     n++
     sql += `   UNION SELECT ${n},'${booking.name}', ${booking.price}\n`
 }
-sql += ')\nWHERE NOT EXISTS(SELECT * FROM BOOKING_POSTIONS);'
+sql += ')\nWHERE NOT EXISTS(SELECT * FROM BOOKING_POSTIONS)'
+*/
+let sql = `DELETE * FROM BOOKING_POSTIONS;\nDELETE * FROM BOOKING;\nINSERT INTO BOOKING (ID, ROOM, GUEST_NAME, FROM_DATE, UNTIL_DATE, PAID)\n  VALUES`
+let n = 1
+for (booking of bookings) {
+    sql +=  ` (${n++}, ${booking.room}, '${booking.name}', '${formatDate(booking.from)}', '${formatDate(booking.to)}', ${Date.now() > booking.to.getTime() ? "'" +formatDate(booking.to) +"'": 'NULL'}'),\n         `
+}
+sql = sql.trim()
+sql = sql.substring(0,sql.length -1)
+sql += `;\nINSERT INTO BOOKING_POSTIONS (BOOKING, NAME, PRICE)\n  VALUES`
+n = 1
+for (booking of bookings) {
+    sql +=  ` (${n++}, 'Zimmer ${booking.room} (${booking.days} Nächte)', ${booking.price}),\n         `
+}
+sql = sql.trim()
+sql = sql.substring(0,sql.length -1)
+sql += ';'
 
 fs.writeFile("bookings.sql", sql, err => {
   if(err) {
      return console.log(err)
   }
-  console.log("The file was saved!");
-});
+  console.log("The file was saved!")
+})
